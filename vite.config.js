fa5f8948e80fa8db8c97aecf8372c6f20c187d41
Isaic Gonzalez <go.isaic@gmail.com>
2024-03-14 00:18:32 -0500

    config update

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,11 @@
-// vite.config.js
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    extensions: [".js", ".jsx"],
-  },
+  // add the server object
   server: {
-    port: 3000, // Change the port number to 3000
+    port: 3000,
   },
 });
